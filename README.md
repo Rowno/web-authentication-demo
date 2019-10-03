5761331cf6c29e3828fc25000604054b664423fd
Roland Warmerdam <rowno@webspirited.com>
2019-10-03 00:25:39 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-authentication
+
+Project description.
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from 'react'
+
+const Home: FunctionComponent = () => {
+  return <div>Welcome to Next.js!</div>
+}
+
+export default Home
