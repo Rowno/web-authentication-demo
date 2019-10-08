78c228bd5389bd90ba8210b680ab140f81ba6893
Roland Warmerdam <rowno@webspirited.com>
2019-10-08 11:17:42 -0700

    Increase challenge size

--- src/server/generate-challenge.ts
+++ src/server/generate-challenge.ts
@@ -2,5 +2,6 @@ import crypto from 'crypto'
 import base64url from 'base64url'
 
 export default function generateChallenge(): string {
-  return base64url.encode(crypto.randomBytes(32))
+  // 64 bytes is the max allowed by the Web Authentication API
+  return base64url.encode(crypto.randomBytes(64))
 }
