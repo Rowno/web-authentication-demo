bf2cdd7a2b02991f1e655a130f3d0665dfabd9e3
Roland Warmerdam <rowno@webspirited.com>
2019-10-08 11:35:55 -0700

    Better material-ui imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import React, { FunctionComponent } from 'react'
 import App from 'next/app'
 import Head from 'next/head'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import IconButton from '@material-ui/core/IconButton'
-import CloseIcon from '@material-ui/icons/Close'
+import { CssBaseline, IconButton } from '@material-ui/core'
+import { Close as CloseIcon } from '@material-ui/icons'
 import { SnackbarProvider, useSnackbar } from 'notistack'
 
 export default class MyApp extends App {
