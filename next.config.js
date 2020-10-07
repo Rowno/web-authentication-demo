85083cb57dfa568d3016dd237afd55d874adca66
Roland Warmerdam <rowno@webspirited.com>
2020-10-07 16:10:14 -0700

    Upgrade dependencies

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const nextTranspileModules = require('next-transpile-modules')
+
+// Transpile the @github/webauthn-json package using Babel because it only has an ES module
+// entrypoint which breaks the Next.js server-side build
+const withTranspileModules = nextTranspileModules(['@github/webauthn-json'])
+
+module.exports = withTranspileModules({
   poweredByHeader: false,
-}
+})
