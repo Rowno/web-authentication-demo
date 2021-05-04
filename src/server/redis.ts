7f366e1749f4354abcc8849633e0a33d3e0f013e
Roland Warmerdam <rowno@webspirited.com>
2021-05-04 23:22:42 +1200

    Fix redis tls

--- src/server/redis.ts
+++ src/server/redis.ts
@@ -1,4 +1,8 @@
 import Redis from 'ioredis'
 import { REDIS_URL, REDIS_TLS_URL } from './config'
 
-export default new Redis(REDIS_TLS_URL ?? REDIS_URL)
+export default new Redis(REDIS_TLS_URL ?? REDIS_URL, {
+  tls: {
+    rejectUnauthorized: false,
+  },
+})
